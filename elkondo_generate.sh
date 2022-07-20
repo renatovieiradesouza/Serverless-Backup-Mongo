dce29561995f9a73eb64dc21de4cbab451bccfcc
Renato Souza <renato@condolivre.com.br>
2022-07-20 17:49:19 -0300

    Debug generate

--- elkondo_generate.sh
+++ elkondo_generate.sh
@@ -14,6 +14,7 @@ elasticuser=$4 #elastic:H8rE2xkLwEuT8VZTueStdQbL
 
 count_log_groups_for_implement=`find temp -name "file.txt" | wc -l`
 date_last_implementd=`date +'%F-%H%M%S'`
+echo $date_last_implementd
 mkdir -p temp/$date_last_implementd
 
 echo
